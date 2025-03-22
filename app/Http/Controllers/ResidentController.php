56b931f44403a2e0a87126395592794b0fa1d51e
nurlaela16 <nurlaelanll16@gmail.com>
2025-03-22 11:39:13 +0700

    templating and index resident

---
+++ app/Http/Controllers/ResidentController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Resident;
+use Illuminate\Validation\Rule;
+
+class ResidentController extends Controller
+{
+    public function index()
+    {
+        $residents = Resident::all();
+        return view('pages.resident.index', [
+            'residents' => $residents,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('pages.resident.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nik' => ['required', 'min:16', 'max:16'],
+            'name' => ['required', 'max:100'],
+            'gender' => ['required', Rule::in(['male', 'female'])],
+            'birth_date' => ['required', 'string'],
+            'birth_place' => ['required', 'max:100'],
+            'addres' => ['required', 'max:700'],
+            'religion' => ['nullable', 'max:50'],
+            'marital_status' => ['required', Rule::in(['single', 'married', 'divorced', 'widowed'])],
+            'occupation' => ['nullable', 'max:100'],
+            'phone' => ['nullable', 'max:15'],
+            'status' => ['required', Rule::in(['active', 'moved', 'deceased'])],
+        ]);
+
+        Resident::create($request->validated());
+        return redirect('/resident')->with('success', 'Berhasil menambahkan data');
+    }
+
+    public function edit($id)
+    {
+        $resident = Resident::findOrFail($id);
+        return view('pages.resident.edit', [
+            'resident' => $resident,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'nik' => ['required', 'min:16', 'max:16'],
+            'name' => ['required', 'max:100'],
+            'gender' => ['required', Rule::in(['male', 'female'])],
+            'birth_date' => ['required', 'string'],
+            'birth_place' => ['required', 'max:100'],
+            'addres' => ['required', 'max:700'],
+            'religion' => ['nullable', 'max:50'],
+            'marital_status' => ['required', Rule::in(['single', 'married', 'divorced', 'widowed'])],
+            'occupation' => ['nullable', 'max:100'],
+            'phone' => ['nullable', 'max:15'],
+            'status' => ['required', Rule::in(['active', 'moved', 'deceased'])],
+        ]);
+
+        Resident::findOrFail($id)->update($request->validated());
+        return redirect('/resident')->with('success', 'Berhasil mengubah data');
+    }
+
+    public function destroy($id)
+    {
+        $resident = Resident::findOrFail($id);
+        $resident->delete();
+
+        return redirect('/resident')->with('succses', 'Berhasil menghapus data');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResidentController;
+
+Route::get('/', function () {
+    return view('layout/app');
+});
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+});
+
+Route::get('/resident', [ResidentController::class, 'index']);
+Route::get('/resident/create', [ResidentController::class, 'create']);
+Route::get('/resident/{id}', [ResidentController::class, 'edit']);
+Route::post('/resident', [ResidentController::class, 'store']);
+Route::put('/resident/{id}', [ResidentController::class, 'update']);
+Route::delete('/resident/{id}', [ResidentController::class, 'delete']);
