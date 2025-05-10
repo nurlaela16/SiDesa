5d544678b0b16dd699dbabf90a73e821dff1372e
nurlaela16 <nurlaelanll16@gmail.com>
2025-05-10 22:10:18 +0700

    autentikasi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        if(Auth::check()){
+            return back();
+        } 
+        return view('pages.auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+         if(Auth::check()){
+            return back();
+        } 
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+           $userStatus = Auth::user()->status;
+           if($userStatus == 'submitted'){
+                return back()->withErrors([
+                    'email' => 'Akun anda masih menunggu persetujuan admin',
+                ]);
+           }else if($userStatus == 'rejected'){
+                return back()->withErrors([
+                    'email' => 'Akun anda telah ditolak admin',
+                ]);
+           }
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'email' => 'Terjadi kesalahan, periksa kembali email dan password anda.',
+        ])->onlyInput('email');
+    }
+
+    public function registerView()
+    {
+         if(Auth::check()){
+            return back();
+        } 
+        return view('pages.auth.register');
+    }
+
+    public function register(Request $request)
+    {
+         if(Auth::check()){
+            return back();
+        } 
+        $validated = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('name'));
+        $user->role_id = 2;
+        $user->saveOrFail();
+
+        return redirect('/')->with('success', 'Berhasil mendaftarkan akun, menunggu persetujuan admin');
+    }
+
+    public function logout(Request $request)
+    {
+         if(!Auth::check()){
+            return redirect('/');
+        } 
+        Auth::logout();
+    
+        $request->session()->invalidate();
+    
+        $request->session()->regenerateToken();
+    
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ResidentController;
 
-Route::get('/', function () {
-    return view('layout/app');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// auth
+Route::get('/', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'authenticate']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/register', [AuthController::class, 'registerView']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/dashboard', function () {
     return view('pages.dashboard');
-});
+})->middleware('role:Admin,User');
 
-Route::get('/resident', [ResidentController::class, 'index']);
-Route::get('/resident/create', [ResidentController::class, 'create']);
-Route::get('/resident/{id}', [ResidentController::class, 'edit']);
-Route::post('/resident', [ResidentController::class, 'store']);
-Route::put('/resident/{id}', [ResidentController::class, 'update']);
-Route::delete('/resident/{id}', [ResidentController::class, 'destroy']);
+Route::get('/resident', [ResidentController::class, 'index'])->middleware('role:Admin');
+Route::get('/resident/create', [ResidentController::class, 'create'])->middleware('role:Admin');
+Route::get('/resident/{id}', [ResidentController::class, 'edit'])->middleware('role:Admin');
+Route::post('/resident', [ResidentController::class, 'store'])->middleware('role:Admin');
+Route::put('/resident/{id}', [ResidentController::class, 'update'])->middleware('role:Admin');
+Route::delete('/resident/{id}', [ResidentController::class, 'destroy'])->middleware('role:Admin');
